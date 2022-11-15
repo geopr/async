8af0b01be1009da22bd29f6545b55f7b4639406c
George Prokhorov <g.prokhorov@bars.group>
2022-11-15 07:37:50 +0300

    modified infrastructure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
-import type { PromisifyValue } from '../index';
+import type { Promisify } from './interface';
+
+export * from './interface';
 
 import { cast } from './utils';
+
+export function async<Data>(data: Data): Promisify<Data>;
+
+export function async<Data>(promise: PromiseLike<Data>): Promisify<Data>;
+
+export function async<Data>(getPromise: () => PromiseLike<Data>): Promisify<Data>;
+
+export function async<Data>(value: Data | PromiseLike<Data> | (() => PromiseLike<Data>)): Promisify<Data> {
+	if (typeof value === 'function') {
+		return cast(proxymify(cast(value)));
+	}
+
+	return cast(proxymify(() => Promise.resolve(value)));
+}
+
+function proxymify<Data>(getData: () => PromiseLike<Data>): unknown {
+	const promise = getData();
+
+	return new Proxy(getData, {
+		get(_, prop) {
+			return handleNativePromise(promise, prop) ?? proxymifyNextValue(promise, prop);
+		},
+
+		apply(target, _, args) {
+			return proxymifyNextValueFromFunctionCall(cast(target), args);
+		},
+	});
+}
 
 function handleNativePromise<Data>(promise: PromiseLike<Data>, prop: string | symbol) {
 	if (!Object.hasOwn(Promise.prototype, prop)) return;
@@ -14,42 +44,21 @@ function handleNativePromise<Data>(promise: PromiseLike<Data>, prop: string | sy
 	return value;
 }
 
+function proxymifyNextValue<Data>(promise: PromiseLike<Data>, prop: string | symbol) {
+	return proxymify(() => getNextValueFromPrevPromise(promise, prop));
+}
+
 async function getNextValueFromPrevPromise<Data>(promise: PromiseLike<Data>, prop: string | symbol): Promise<unknown> {
 	const data = await promise;
 	const value = data[cast<keyof Data>(prop)];
 	return typeof value === 'function' ? value.bind(data) : value;
 }
 
-function proxymify<Data>(getData: () => PromiseLike<Data>): unknown {
-	const promise = getData();
-
-	return new Proxy(getData, {
-		get(_, prop) {
-			return (
-				handleNativePromise(promise, prop)
-				?? proxymify(() => getNextValueFromPrevPromise(promise, prop))
-			);
-		},
-
-		apply(target, _, args) {
-			return proxymify(async () => {
-				const fn = await target();
-				return cast<Function>(fn)(...args);
-			});
-		},
-	});
+function proxymifyNextValueFromFunctionCall(getFn: () => Promise<Function>, args: any[]) {
+	return proxymify(() => getNextValueFromFunction(getFn, args));
 }
 
-export function async<Data>(data: Data): PromisifyValue<Data>;
-
-export function async<Data>(promise: PromiseLike<Data>): PromisifyValue<Data>;
-
-export function async<Data>(getPromise: () => PromiseLike<Data>): PromisifyValue<Data>;
-
-export function async<Data>(value: Data | PromiseLike<Data> | (() => PromiseLike<Data>)): PromisifyValue<Data> {
-	if (typeof value === 'function') {
-		return cast(proxymify(cast(value)));
-	}
-
-	return cast(proxymify(() => Promise.resolve(value)));
+async function getNextValueFromFunction(getFn: () => Promise<Function>, args: any[]) {
+	const fn = await getFn();
+	return fn(...args);
 }
--- src/interface.ts
+++ src/interface.ts
@@ -1,12 +1,12 @@
-type Fn<Return, Args> = <
+type Fn<Return, Args extends any[]> = <
 	R extends Return = Return,
 	A extends Args = Args,
->(...args: A) => PromisifyValue<R>;
+>(...args: A) => Promisify<R>;
 
 type AnyFunction = (...params: any[]) => any;
 
 type Overloads<T> = T extends () => infer R
-	? <Return extends R = Return>() => PromisifyValue<Return>
+	? <Return extends R = R>() => Promisify<Return>
 
 	: T extends {
 			(...args: infer A1): infer R1;
@@ -19,26 +19,26 @@ type Overloads<T> = T extends () => infer R
 			Fn<R3, A3> |
 			Fn<R4, A4>
 
-	: T extends {
-			(...args: infer A1): infer R1;
-			(...args: infer A2): infer R2;
-			(...args: infer A3): infer R3;
-		}
-	? Fn<R1, A1> |
-		Fn<R2, A2> |
-		Fn<R3, A3>
+		: T extends {
+				(...args: infer A1): infer R1;
+				(...args: infer A2): infer R2;
+				(...args: infer A3): infer R3;
+			}
+		? Fn<R1, A1> |
+			Fn<R2, A2> |
+			Fn<R3, A3>
 
-	: T extends {
-			(...args: infer A1): infer R1;
-			(...args: infer A2): infer R2;
-		}
-	? Fn<R1, A1> |
-		Fn<R2, A2>
+		: T extends {
+				(...args: infer A1): infer R1;
+				(...args: infer A2): infer R2;
+			}
+		? Fn<R1, A1> |
+			Fn<R2, A2>
 
-	: T extends (...args: infer A1) => infer R1
-	? Fn<R1, A1>
+		: T extends (...args: infer A1) => infer R1
+		? Fn<R1, A1>
 
-	: never;
+		: never;
 
 type UnionToIntersection<Fn> =
 	(Fn extends any ? (fn: Fn) => void : never) extends (fn: infer F) => void ? F : never;
@@ -63,22 +63,10 @@ type PromisifySchema<Schema, Origin> = WithPromise<
 	{
 		[Key in keyof Schema]: Schema[Key] extends AnyFunction
 			? UnionToIntersection<Overloads<Schema[Key]>>
-			: PromisifyValue<Schema[Key]>;
+			: Promisify<Schema[Key]>;
 	},
 	Origin
 >;
 
-export type PromisifyValue<Value> = PromisifySchema<GetSchema<Value>, Value>;
-
-declare namespace async {
-	type Promisify<Value> = PromisifyValue<Value>
-
-	function async<Data>(data: Data): Promisify<Data>;
-
-	function async<Data>(promise: PromiseLike<Data>): Promisify<Data>;
-
-	function async<Data>(getPromise: () => PromiseLike<Data>): Promisify<Data>;
-}
-
-export = async;
+export type Promisify<Value> = PromisifySchema<GetSchema<Value>, Value>;
 
